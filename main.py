c5cdc821941da207423c62ad902a53ef5b83cf72
Steven Zhivov <stevenzhivov1@gmail.com>
2023-07-09 03:55:46 -0400

    initial opening of chrome browser with first steps

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+query = input("What would you like to search: ")
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com")
+
+
+gInput = driver.find_element(By.XPATH, "//textarea[@title='Search']")
+gInput.send_keys(query)
+gInput.send_keys(Keys.RETURN)
+
+moreBtn = driver.find_element(By.XPATH, '//*[@id="rso"]/div[2]/div/div/div[1]/div[5]/div/div[1]/a/div/span[1]')
+moreBtn.click()
+time.sleep(60)
