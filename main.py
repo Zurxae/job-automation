c452a92b8c741f80b859e7ab820daa267b4b597d
Steven Zhivov <stevenzhivov1@gmail.com>
2023-10-01 01:43:19 -0400

    EVERYTHING FUCKING WORKS (i think...plz work)

--- main.py
+++ main.py
@@ -15,25 +15,33 @@ OUTPUT_FILE = 'output.txt'
 # Program start time
 st = time.time()
 
-queryList = get_queries(input_file=INPUT_FILE)
+queryList, bad_emails = get_queries(input_file=INPUT_FILE)
 
 # Create Chrome Driver instance
 options = webdriver.ChromeOptions()
+options.add_argument('--headless')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(options=options)
 
 # To clear existing file or make new one
 with open(OUTPUT_FILE, 'w'):
     pass
 
+total_emails = 0
+
 for query in queryList:
+    driver = webdriver.Chrome(options=options)
+    driver.set_page_load_timeout(2)
+    driver.implicitly_wait(1)
+    
     search_query(driver=driver, query=query)
     webLinks = get_web_links(driver=driver)
-    execute_query(query=query, webLinks=webLinks, output_file=OUTPUT_FILE)
+    total_emails += execute_query(driver=driver, query=query, webLinks=webLinks, bad_emails=bad_emails)
 
-
-driver.quit()
-
+    driver.quit()
 
 elapsed_time = time.time() - st
 print('Total Execution time:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+print(f'Total emails found: {total_emails}')
+with open(OUTPUT_FILE, 'a') as fout:
+    fout.write('Total Execution time:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)), '\n')
+    fout.write(f'Total emails found: {total_emails}')
